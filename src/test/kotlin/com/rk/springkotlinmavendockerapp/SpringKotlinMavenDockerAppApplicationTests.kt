5247f240b943bfa5056a18927b0847c8526eabdd
Radek Krizka <radek.krizka@pricefx.com>
2025-03-23 12:10:13 +0100

    Initial commit

---
+++ src/test/kotlin/com/rk/springkotlinmavendockerapp/SpringKotlinMavenDockerAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rk.springkotlinmavendockerapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinMavenDockerAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
